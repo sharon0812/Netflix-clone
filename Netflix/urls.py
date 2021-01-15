09811b1c3143414f1ffae9b65d380b5f21d555c4
sharon0812 <anyangosharon@gmail.com>
2021-01-15 13:23:58 +0300

    first commit

--- Netflix/urls.py
+++ Netflix/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.conf.urls import include
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     path('',include('netflixApi.urls'))
 ]
